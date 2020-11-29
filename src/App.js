d6df07283afb42c747be77097d70d0281b342b83
Ubuntu <ubuntu@ip-172-31-37-179.ap-northeast-2.compute.internal>
2020-11-29 17:44:57 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learns React
         </a>
       </header>
     </div>
